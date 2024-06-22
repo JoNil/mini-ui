d5702961a3ba9763d519534124d04c630bf09754
Jonathan Nilsson <lord.jonil@gmail.com>
2024-06-22 02:18:08 +0200

    Maybe cairo is the way

--- examples/tiny.rs
+++ examples/tiny.rs
@@ -1,3 +1,6 @@
+use core::slice;
+
+use cairo::{Context, Format, ImageSurface};
 use mini_ui::window::{Key, Window, WindowOptions};
 
 const WIDTH: usize = 640;
@@ -12,11 +15,36 @@ fn main() {
     )
     .unwrap();
 
-    let buffer = vec![0xffff_ffff; WIDTH * HEIGHT];
+    let mut surface = ImageSurface::create(Format::ARgb32, WIDTH as _, HEIGHT as _)
+        .expect("Can't create surface");
+
+    {
+        let context = Context::new(&surface).unwrap();
+
+        context.set_source_rgb(1.0, 1.0, 1.0);
+        context.paint();
+
+        context.select_font_face(
+            "Helvetica",
+            cairo::FontSlant::Normal,
+            cairo::FontWeight::Normal,
+        );
+        context.set_font_size(40.0);
+        context.set_source_rgb(0.0, 0.0, 0.0);
+        context.move_to(50.0, 100.0);
+        context.show_text("Hello, Cairo!");
+    }
 
     window.set_target_fps(60);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        let data = surface.data().unwrap();
+        window
+            .update_with_buffer(
+                unsafe { slice::from_raw_parts(data.as_ptr() as *const _, data.len() / 4) },
+                WIDTH,
+                HEIGHT,
+            )
+            .unwrap();
     }
 }
