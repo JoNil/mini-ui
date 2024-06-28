ae2e794b6fbe45f6e362c83d312ff9686838c5ec
Jonathan Nilsson <jonathan@voysys.se>
2024-06-28 17:57:21 +0200

    Swap colors

--- src/ui/image.rs
+++ src/ui/image.rs
@@ -43,8 +43,14 @@ impl Image {
         let mut image = ImageSurface::create(Format::ARgb32, width, height).unwrap();
 
         {
-            let mut image_date = image.data().unwrap();
-            image_date.copy_from_slice(data);
+            let mut image_data = image.data().unwrap();
+
+            for (dst, src) in image_data.chunks_mut(4).zip(data.chunks(4)) {
+                dst[0] = src[2];
+                dst[1] = src[1];
+                dst[2] = src[0];
+                dst[3] = src[3];
+            }
         }
 
         Image {
